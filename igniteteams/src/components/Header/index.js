25d33e4c2efe8c36bb90a1b67d3f593d532bb2cb
Víctor Barreto <victor.barreto@behoh.com>
2023-03-21 18:11:19 -0300

    Criando componente Button Icon

--- igniteteams/src/components/Header/index.js
+++ igniteteams/src/components/Header/index.js
@@ -1,14 +1,15 @@
 import { BackIcon, Container, Logo, BackButton } from "./styles";
 import logoImg from "../../assets/logo.png";
 
-export function Header({showBackButton = false}) {
-
+export function Header({ showBackButton = false }) {
     return (
         <Container>
-            {showBackButton && <BackButton>
-                <BackIcon />
-            </BackButton>}
-            
+            {showBackButton && (
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            )}
+
             <Logo source={logoImg} />
         </Container>
     );
