2007d81f622155d83eca6ce4f0b94ffcb95bd570
Víctor Barreto <victorbarretolins@gmail.com>
2023-06-07 18:08:38 -0300

    Input de dados

--- imhere/src/screens/Home/styles.ts
+++ imhere/src/screens/Home/styles.ts
@@ -16,4 +16,11 @@ export const styles = StyleSheet.create({
 		color: "#6B6B6B",
 		fontSize: 16,
 	},
+	input: {
+		backgroundColor: "#1F1E25",
+		borderRadius: 5,
+		color: "#FFF",
+		padding: 16,
+		fontSize: 16,
+	},
 });
