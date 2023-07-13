5a3c37403555045e77661cfcce496d496ac82263
Víctor Barreto <victor.barreto@behoh.com>
2023-07-13 18:00:00 -0300

    deixando o ícone dinâmico

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -1,17 +1,25 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Imput";
 
 export function Players() {
-	return (
-		<Container>
-			<Header showBackButton />
+    return (
+        <Container>
+            <Header showBackButton />
 
-			<Highlight title="Nome da turma" subtitle="adicione a galera e separe os times" />
+            <Highlight
+                title="Nome da turma"
+                subtitle="adicione a galera e separe os times"
+            />
 
-			<ButtonIcon />
-		</Container>
-	);
+            <Form>
+                <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
+                <ButtonIcon icon="add" />
+            </Form>
+        </Container>
+    );
 }
